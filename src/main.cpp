7a2eddca932717edaac8b47b47677b89de8f2f37
nicholasbenaya <nicholasbenaya17@gmail.com>
2025-09-18 04:18:55 +0700

    added fps indicator

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,20 @@
 
 using namespace std;
 int main() {
-  sf::RenderWindow window(sf::VideoMode(800, 600), "Collision Sim");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Collision Sim");
+//   sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "Collision Sim",
+//                           sf::Style::Fullscreen);
+  sf::Font font;
+  if (!font.loadFromFile("/mnt/c/Users/mahdi/Desktop/Dasprog/Collision "
+                         "SImulation/assets/font/MontserratBlack-3zOvZ.ttf")) {
+    std::cerr << "Failed to load font\n";
+    return -1;
+  }
+  sf::Text fpsText;
+  fpsText.setFont(font);
+  fpsText.setCharacterSize(20);
+  fpsText.setFillColor(sf::Color::Green);
+  fpsText.setPosition(window.getSize().x - 100, 10); // Top-right corner
 
   // Particle object creation
   Particle p1(0, 0, 5.0);
@@ -15,13 +28,15 @@ int main() {
   sf::CircleShape shape1(sqrt(p1.getMass()) *
                          20); // Radius proportional to mass
   shape1.setFillColor(sf::Color::Red);
-  shape1.setOrigin(shape1.getRadius(), shape1.getRadius()); // Set origin to center  
+  shape1.setOrigin(shape1.getRadius(),
+                   shape1.getRadius()); // Set origin to center
   shape1.setPosition(p1.getX(), p1.getY());
 
   sf::CircleShape shape2(sqrt(p2.getMass()) *
                          20); // Radius proportional to mass
   shape2.setFillColor(sf::Color::Blue);
-    shape2.setOrigin(shape2.getRadius(), shape2.getRadius()); // Set origin to center
+  shape2.setOrigin(shape2.getRadius(),
+                   shape2.getRadius()); // Set origin to center
   shape2.setPosition(p2.getX(), p2.getY());
 
   sf::Clock clock;
@@ -35,7 +50,9 @@ int main() {
     double dt = clock.restart().asSeconds();
     if (dt > 0.5)
       dt = 0.5; // clamp to avoid huge jumps
-    
+    double fps = 1.0 / dt;
+    fpsText.setString("FPS: " + std::to_string(static_cast<int>(fps)));
+
     // p1.update(1/60.0, window.getSize().x, window.getSize().y);
     p1.update(dt, window.getSize().x, window.getSize().y);
     shape1.setPosition(p1.getX(), p1.getY());
@@ -52,6 +69,7 @@ int main() {
     window.clear();
     window.draw(shape1);
     window.draw(shape2);
+    window.draw(fpsText);
     window.display();
   }
   return 0;
